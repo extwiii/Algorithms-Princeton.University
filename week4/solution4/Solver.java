70ff4c121720f598875dcdb32a6d9c9d4b03f443
extwiii <bcagiran@hotmail.com>
2016-12-29 22:07:37 +0000

    add week4 and week 5 solutions

---
+++ week4/solution4/Solver.java
@@ -0,0 +1,139 @@
+import edu.princeton.cs.algs4.*;
+
+public class Solver {
+    private static class SearchNode implements Comparable<SearchNode> {
+        final Board board;
+        final int moves;
+        final SearchNode previous;
+        SearchNode(Board board, int moves, SearchNode previous) {
+            this.board = board;
+            this.moves = moves;
+            this.previous = previous;
+        }
+
+        Iterable<Board> iterable() {
+            SearchNode previous = this.previous;
+            Stack<Board> boards = new Stack<Board>();
+            boards.push(this.board);
+            while (previous != null) {
+                boards.push(previous.board);
+                previous = previous.previous;
+            }
+            return boards;
+        }
+
+        @Override
+        public int compareTo(SearchNode that) {
+            return moves + board.manhattan() - (that.moves + that.board.manhattan());
+        }
+    }
+
+    private final MinPQ<SearchNode> nodes;
+    private final MinPQ<SearchNode> twins;
+
+    private final SearchNode initialNode;
+    private SearchNode solution;
+    private SearchNode solutionTwin;
+
+    /**
+     * Find a solution to the initial board (using the A* algorithm)
+     * @param initial
+     */
+    public Solver(Board initial) {
+        nodes = new MinPQ<Solver.SearchNode>();
+        twins = new MinPQ<Solver.SearchNode>();
+        initialNode = new SearchNode(initial, 0, null);
+
+        solution = null;
+        solutionTwin = null;
+
+        nodes.insert(initialNode);
+        twins.insert(new SearchNode(initial.twin(), 0, null));
+    }
+
+    /**
+     * Is the initial board solvable?
+     * @return true iff solvable
+     */
+    public boolean isSolvable()  {
+        if (initialNode.board.dimension() < 2) return false;
+        if (initialNode.board.isGoal()) {
+            solution = initialNode;
+            return true;
+        }
+
+        if (solution != null) return true;
+        if (solutionTwin != null) return false;
+
+        while (true) {
+            SearchNode node = nodes.delMin();
+            SearchNode twin = twins.delMin();
+
+            if (node.board.isGoal()) {
+                solution = node;
+                return true;
+            }
+
+            if (twin.board.isGoal()) {
+                solutionTwin = twin;
+                return false;
+            }
+
+            processNeighbours(node, node.board.neighbors(), nodes);
+            processNeighbours(twin, twin.board.neighbors(), twins);
+        }
+    }
+
+    /**
+     * Min number of moves to solve initial board; -1 if unsolvable
+     * @return min number of moves to solve initial board; -1 if unsolvable
+     */
+    public int moves() {
+        if (solution == null && !isSolvable()) return -1;
+        return solution.moves;
+    }
+
+    /**
+     * Sequence of boards in a shortest solution; null if unsolvable
+     * @return solutions
+     */
+    public Iterable<Board> solution() {
+        if (solution == null && !isSolvable()) return null;
+        return solution.iterable();
+    }
+
+    private void processNeighbours(SearchNode node, Iterable<Board> neighbors, MinPQ<SearchNode> queue) {
+        for (Board neignour: node.board.neighbors()) {
+            if (alreadyPassed(neignour, node)) continue;
+            queue.insert(new SearchNode(neignour, node.moves + 1, node));
+        }
+    }
+
+    private boolean alreadyPassed(Board current, SearchNode parent) {
+        if (parent.previous == null) return false;
+        return current.equals(parent.previous.board);
+    }
+
+    public static void main(String[] args) {
+        // create initial board from file
+        In in = new In(args[0]);
+        int N = in.readInt();
+        int[][] blocks = new int[N][N];
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                blocks[i][j] = in.readInt();
+        Board initial = new Board(blocks);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
+    }
+}
